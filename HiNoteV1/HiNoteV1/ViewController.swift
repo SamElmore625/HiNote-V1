8d793553c06d24738fc3aa9a8f64f028bfb7231c
Dev Jijilal <35881411+devjij@users.noreply.github.com>
2020-12-05 23:48:30 -0500

    Add files via upload

---
+++ HiNoteV1/HiNoteV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiNoteV1
+//
+//  Created by Dev Jijilal on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
